5fcb88b2e3965dd4d092cbb6ab71336d10050909
ike imbanu <imbanu1@gmail.com>
2024-01-16 09:27:20 -0400

    added to index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const fs = require ('fs');
+const inquirer = require('inquirer');
 
+const generatePage = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer.prompt([
+    'What is the title of your project ?'
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
